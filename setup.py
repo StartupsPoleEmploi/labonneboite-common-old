49b8aa924857121009bf1c06b3cdca4d3f257621
Léni Marvaud <24732919+lmarvaud@users.noreply.github.com>
2022-04-13 15:57:52 +0200

    📦️ add sub-package to the library

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+packages = find_packages(exclude=('labonneboite_common.tests',))
+
 setup(
     name="labonneboite-common",
-    version="0.1",
+    version="0.1.1",
     author="La Bonne Boite - common library",
     author_email="labonneboite@pole-emploi.fr",
     description=(""),
-    packages=['labonneboite_common', ],
+    packages=packages,
     include_package_data=True,
     long_description=read('README.md'),
     install_requires=[req for req in open('requirements.txt')],
